ffa7516147330787f1c1f2a2fbe2ff64296d8abc
IT26101835 <it26101835@my.sliit.lk>
2026-08-30 20:01:08 +0530

    Complete Part 1-B simulation 1 part

--- main.c
+++ main.c
@@ -60,11 +60,12 @@ int battleshipSunk = 0;
 int sinkingEscortIndex = -1;
 int hitEscortCount = 0;
 float totalBattleTime = 0;
-
+    //use for part 1b simulation
 int k;
 int pathX[100];
 int pathY[100];
-float pointBattleTime = 0;
+int pointHitCount;
+float pointBattleTime;
     FILE *file; // File pointer for saving battle conditions
 
     BattleShip B; //create a battleship variable
@@ -263,6 +264,8 @@ sinkingEscortIndex = -1;
             printf("Path Point %d = (%d, %d)\n",i + 1, pathX[i], pathY[i]);
         }
         for (int i = 0; i < k; i++){     // Move the battleship to each path point
+            pointHitCount = 0;
+            pointBattleTime = 0;
             B.x = pathX[i];
             B.y = pathY[i];
             printf("\nBattleship moved to Point %d\n", i + 1);
@@ -273,8 +276,9 @@ sinkingEscortIndex = -1;
         
             for (int j = 0; j < n; j++){ // Check only escort ships that are still active
                 if (E[j].isAlive == 1){
+                   // E[j].maxAttackRange = 0;  
                     E[j].distanceFromB = CalculateDistance(B.x, B.y, E[j].x, E[j].y); // Recalculate the distance from the new battleship position
-                // Check if the battleship and escort ship can hit each other
+                // Check if the battleship and esscort ship can hit each other
                     E[j].canBeHitByB = CanBattleshipHit(E[j].distanceFromB, B.maxAttackRange);
                     E[j].canHitB = CanEscortHitBattleship(E[j].distanceFromB, E[j].maxAttackRange);
                     printf("Distance to E%d = %.2f\n",E[j].index, E[j].distanceFromB);
@@ -300,12 +304,18 @@ sinkingEscortIndex = -1;
                                 battleshipSunk = 1;
                                 sinkingEscortIndex = E[j].index;
                                 printf("Battleship was sunk by Escort Ship E%d\n",E[j].index);
+                                fprintf(file, "Result = BATTLESHIP SUNK\n");
+                                fprintf(file, "Battleship was sunk by Escort Ship E%d\n\n",E[j].index);
                                 break;
                         }
                          if (E[j].canBeHitByB == 1){         // Destroy the escort ship if B can hit it
                                 E[j].firingAngleFromB = CalculateFiringAngle(E[j].distanceFromB,B.maxVelocity);
                                 E[j].timeToHitFromB = CalculateTimeToHit(E[j].distanceFromB,B.maxVelocity,E[j].firingAngleFromB);
+                                if (E[j].timeToHitFromB > pointBattleTime){
+                                    pointBattleTime = E[j].timeToHitFromB;
+                                }
                                 E[j].isAlive = 0;
+                                pointHitCount++;
                                 printf("Escort Ship E%d was destroyed by B\n",E[j].index);
                                 printf("Time to Hit = %.2f seconds\n",E[j].timeToHitFromB);
                                 fprintf(file, "Result = DESTROYED BY B\n");
@@ -313,11 +323,34 @@ sinkingEscortIndex = -1;
                         }
                 }
             }
+            if (battleshipSunk == 0){     // Save results if B survived this path point
+                fprintf(file, "Battleship SURVIVED this point\n");
+                fprintf(file, "Number of Escort Ships hit by B = %d\n\n",pointHitCount);
+                fprintf(file, "Battle Time at this Point = %.2f seconds\n\n",pointBattleTime);
+            }
             if (battleshipSunk == 1){     // Stop the simulation if the battleship is sunk
                 break;
             }
         }
+        fprintf(file, "\n=== FINAL SIMULATION 1 STATUS ===\n");
+        if (battleshipSunk == 1){
+            fprintf(file, "Battleship Status = SUNK\n");
+            fprintf(file, "Sunk by Escort Ship E%d\n",sinkingEscortIndex);
+        }
+        else{
+            fprintf(file, "Battleship Status = SURVIVED\n");
+        }
+        fprintf(file, "\nEscort Ships Final Status:\n");
 
+        for (int i = 0; i < n; i++){
+            if (E[i].isAlive == 1){
+                fprintf(file, "Escort Ship E%d = ALIVE\n",E[i].index);
+            }
+            else{
+                fprintf(file, "Escort Ship E%d = DESTROYED\n",E[i].index);
+            }
+        }   
+    fclose(file);
     return 0;
     
 }
